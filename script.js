0753bd5d7dc1edf9345f95344990980eee5b7c94
NAAZ <160319806+Nazia-Naaz@users.noreply.github.com>
2024-06-25 16:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,36 @@ function init() {
 
 init()
 
+
+
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init()
+
 function firstFageAnimation(){
     var tl = gsap.timeline()
     tl.from("#main",{
@@ -70,7 +100,34 @@ function firstFageAnimation(){
 firstFageAnimation()
 
 
-
+var slide1h1 = document.querySelectorAll('#page2 .slide1h1 h1');
+slide1h1.forEach(function (elem) {
+  gsap.to(elem, {
+    transform: 'translateX(-96%)',
+    ease: 'linear',
+    duration: 4,
+    scrollTrigger: {
+      trigger: '#page2',
+      scroller: '#main',
+       /*markers: true,*/
+      scrub: 3
+    }
+  })
+})
+var slide2h1 = document.querySelectorAll('#page2 .slide2h1 h1');
+slide2h1.forEach(function (elem) {
+  gsap.to(elem, {
+    transform: 'translateX(0%)',
+    ease: 'linear',
+    duration: 4,
+    scrollTrigger: {
+      trigger: '#page2',
+      scroller: '#main',
+      // markers: true,
+      scrub: 3
+    }
+  })
+})
 
 
 
